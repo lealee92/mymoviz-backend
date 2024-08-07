b0933350f6c8e49f6f5dc5108c0d6b121100f035
Lea <lee.lea92@gmail.com>
2024-08-07 15:58:18 +0200

    correction

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 var express = require("express");
 var router = express.Router();
 const fetch = require("node-fetch");
-const url = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.OWM_API_KEY}`;
-const options = { method: "GET", headers: { accept: "application/json" } };
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 router.get("/movies", (req, res) => {
-  fetch(url, options)
+  fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
     .then((response) => response.json())
     .then((data) => {
-      res.json(data);
+      res.json({ movies: data.results });
     });
 });
 
